4aed1aefbc8f4ebe20ffa3c638c128a7a60230bc
Michael Meoak <mmeoak240@gmail.com>
2022-03-01 11:06:48 -0700

    commit tree 684507fd8a10e27854cdfaf43f5a70d92a4f9478

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+ bestCustomer = 'not bob'
+}
+
+overwriteBestCustomer = function () {
+   bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'jerry'
+
+function changeLeastFavoriteCustomer() {
+  return leastFavoriteCustomer = 'michael'
+}
